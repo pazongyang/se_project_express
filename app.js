139784ed612e7844a7890aa984962458f179dc65
Pazongyang <pazongyang987@gmail.com>
2025-10-04 01:25:41 -0500

    fourth submission

--- app.js
+++ app.js
@@ -1,69 +1,28 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const mainRouter = require("./routes/index");
-// const { SERVER_ERROR } = require("./utils/errors");
-
-// const app = express();
-// const { PORT = 3001 } = process.env;
-
-// mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch((err) => {
-//   console.error("DB connection error:", err);
-//   process.exit(1);
-// });
-
-// app.use(express.json());
-
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: "68df1a1caef3cf37fff9365c",
-//   };
-//   next();
-// });
-
-// app.use("/", mainRouter);
-
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res
-//     .status(SERVER_ERROR)
-//     .send({ message: "An error has occurred on the server." });
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
-const { SERVER_ERROR } = require("./utils/errors");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch((err) => {
-  console.error("DB connection error:", err);
-  process.exit(1);
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
 
 app.use(express.json());
 
-app.use((req, res, _next) => {
+app.use((req, res, next) => {
   req.user = {
     _id: "68df1a1caef3cf37fff9365c",
   };
-  _next();
+  next();
 });
 
 app.use("/", mainRouter);
 
-app.use((err, req, res, _next) => {
-  console.error(err);
-  res
-    .status(SERVER_ERROR)
-    .send({ message: "An error has occurred on the server." });
-});
-
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
