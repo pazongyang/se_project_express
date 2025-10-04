be00109e81238369419bacfe7b6c2a338b7f873a
Pazongyang <pazongyang987@gmail.com>
2025-10-04 01:18:36 -0500

    third submission

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const mainRouter = require("./routes/index");
+// const { SERVER_ERROR } = require("./utils/errors");
+
+// const app = express();
+// const { PORT = 3001 } = process.env;
+
+// mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch((err) => {
+//   console.error("DB connection error:", err);
+//   process.exit(1);
+// });
+
+// app.use(express.json());
+
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: "68df1a1caef3cf37fff9365c",
+//   };
+//   next();
+// });
+
+// app.use("/", mainRouter);
+
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res
+//     .status(SERVER_ERROR)
+//     .send({ message: "An error has occurred on the server." });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
@@ -13,16 +48,16 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch((err) => {
 
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req, res, _next) => {
   req.user = {
     _id: "68df1a1caef3cf37fff9365c",
   };
-  next();
+  _next();
 });
 
 app.use("/", mainRouter);
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, _next) => {
   console.error(err);
   res
     .status(SERVER_ERROR)
