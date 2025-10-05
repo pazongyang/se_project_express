a2735a5c2b8d6650df38c5641d648a8559ad9a92
Pazongyang <pazongyang987@gmail.com>
2025-10-04 21:26:11 -0500

    first submission fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,14 @@
 const User = require("../models/user");
+const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -16,9 +19,11 @@ const createUser = (req, res) => {
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: "Invalid user data" });
+        return res.status(BAD_REQUEST).send({ message: "Invalid user data." });
       }
-      return res.status(500).send({ message: "Internal server error" });
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -30,12 +35,16 @@ const getUser = (req, res) => {
     .catch((err) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: "User not found" });
+        return res.status(NOT_FOUND).send({ message: "User not found." });
       }
       if (err.name === "CastError") {
-        return res.status(400).send({ message: "Invalid user ID format" });
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Invalid user ID format." });
       }
-      return res.status(500).send({ message: "Internal server error" });
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error has occurred on the server." });
     });
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { NOT_FOUND } = require("../utils/errors");
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
 
@@ -6,7 +7,7 @@ router.use("/users", userRouter);
 router.use("/items", clothingItemsRouter);
 
 router.use((req, res) => {
-  res.status(404).send({ message: "Requested resource not found" });
+  res.status(NOT_FOUND).send({ message: "Requested resource not found." });
 });
 
 module.exports = router;
