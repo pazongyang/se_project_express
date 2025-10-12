2ab4eb8f0c4fe1b79043665f96eef174e707428d
Pazongyang <pazongyang987@gmail.com>
2025-10-12 10:24:15 -0500

    arguements

--- app.js
+++ app.js
@@ -14,13 +14,20 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch((err) => {
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.use("/", mainRouter);
 
 app.use((req, res) => {
   res.status(NOT_FOUND).json({ message: "Requested resource not found" });
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err);
   res
     .status(SERVER_ERROR)
