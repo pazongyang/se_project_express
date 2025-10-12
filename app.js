29043d57d8d7b52f752155ff509a5f13590d9025
Pazongyang <pazongyang987@gmail.com>
2025-10-12 10:59:08 -0500

    test endpoint errors

--- app.js
+++ app.js
@@ -14,20 +14,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch((err) => {
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
-  };
-  next();
-});
-
 app.use("/", mainRouter);
 
 app.use((req, res) => {
   res.status(NOT_FOUND).json({ message: "Requested resource not found" });
 });
 
-// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.error(err);
   res
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,18 @@
 const router = require("express").Router();
-const { NOT_FOUND } = require("../utils/errors");
 const { login, createUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
-
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
 
-router.post("/signup", createUser);
 router.post("/signin", login);
-
-router.use("/items", clothingItemsRouter);
+router.post("/signup", createUser);
 
 router.use(auth);
 router.use("/users", userRouter);
+router.use("/items", clothingItemsRouter);
 
 router.use((req, res) => {
-  res.status(NOT_FOUND).send({ message: "Requested resource not found." });
+  res.status(404).json({ message: "Requested resource not found" });
 });
 
 module.exports = router;
